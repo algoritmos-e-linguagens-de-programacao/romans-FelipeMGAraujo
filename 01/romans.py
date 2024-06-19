8f1181a6fb7b4ec72054af9587cb3d41d5359acd
Felipe Araujo <168040742+fearaujo1@users.noreply.github.com>
2024-06-18 21:55:39 -0300

    Update romans.py

--- 01/romans.py
+++ 01/romans.py
@@ -1,44 +1,26 @@
 def int_to_roman(num):
     
-    mil = [
-        "", "M", 
-        "MM", "MMM"
-    ]
-    cem = [
-        "", "C", "CC", "CCC", 
-        "CD", "D", "DC", 
-        "DCC", "DCCC", 
-        "CM"
-    ]
-    dez = [
-        "", "X", "XX", "XXX",
-        "XL", "L", "LX", 
-        "LXX", "LXXX", 
-        "XC"
-    ]
-    uni = [
-        "", "I", "II", "III", 
-        "IV", "V", "VI", 
-        "VII", "VIII", 
-        "IX"
-    ]
+    milhar = ["", "M", "MM", "MMM"]
+    centena = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+    dezena = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+    unidade = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
 
-    return mil[num // 1000] + cem[(num % 1000) // 100] + dez[(num % 100) // 10] + uni[num % 10]
+    return milhar[num // 1000] + centena[(num % 1000) // 100] + dezena[(num % 100) // 10] + unidade[num % 10]
 
 def roman_to_int(s):
     
-    romano_inteiro = { 'I': 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000 }
+    roman_int = { 'I': 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000 }
     total = 0
-    x = 0
+    i = 0
 
-    for x in range( len(s) ):
-        if x + 1 < len(s) and romano_inteiro[s[x]] < romano_inteiro[s[x + 1]]:
-            total -= romano_inteiro[s[x]]
+    for i in range( len(s) ):
+        if i + 1 < len(s) and roman_int[s[i]] < roman_int[s[i + 1]]:
+            total -= roman_int[s[i]]
 
         else:
-            total += romano_inteiro[s[x]]
+            total += roman_int[s[i]]
 
     return total
 
-#print( inteiro_para_romano(2006) )
-#print( romano_para_inteiro('MMVI') )
+#print( int_to_roman(2002) )
+#print( roman_to_int('MMII') )
